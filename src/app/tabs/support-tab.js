583ab533e670286ea7dd3f1385efb6d07f2819af
Kris Jenkins <krisajenkins@gmail.com>
2018-07-27 16:54:15 +0100

    CGKIELE-537: Update the support to to include (only) IOHK official channels.

--- src/app/tabs/support-tab.js
+++ src/app/tabs/support-tab.js
@@ -13,15 +13,13 @@ module.exports = class SupportTab {
     self._api = api
     self._events = events
     self._opts = opts
-    self._view = { el: null, gitterIframe: '' }
-    self.data = { gitterIsLoaded: false }
+    self._view = { el: null }
+    self.data = {}
     self._components = {}
     self._events.app.register('tabChanged', (tabName) => {
-      if (tabName !== 'Support' || self.data.gitterIsLoaded) return
-      if (!self._view.gitterIframe) self._view.gitterIframe = yo`<iframe class="${css.chatIframe}" src='https://gitter.im/ethereum/remix/~embed'>`
+      if (tabName !== 'Support') return
       yo.update(self._view.el, self.render())
       self._view.el.style.display = 'block'
-      self.data.gitterIsLoaded = true
     })
   }
   render () {
@@ -29,21 +27,19 @@ module.exports = class SupportTab {
     var el = yo`
       <div class="${css.supportTabView}" id="supportView">
         <div class="${css.infoBox}">
-          Have a question, found a bug or want to propose a feature? Have a look at the
-          <a class="${css.linkStyle}" target="_blank" href='https://github.com/ethereum/browser-solidity/issues'> issues</a> or check out
-          <a class="${css.linkStyle}" target="_blank" href='https://remix.readthedocs.io/en/latest/'> the documentation page on Remix</a> or
-          <a class="${css.linkStyle}" target="_blank" href='https://solidity.readthedocs.io/en/latest/'> Solidity</a>.
-        </div>
-        <div class="${css.chat}">
-          <div class="${css.chatTitle}" onclick=${openLink} title='Click to open chat in Gitter'>
-            <div class="${css.chatTitleText}">ethereum/remix community chat</div>
-          </div>
-          ${self._view.gitterIframe}
+          <p>Your feedback is important because it helps us improve IELE
+          and the testnet. We have set up several communication
+          channels to help with the process:</p>
+
+          <ul class="${css.linkList}">
+            <li>Telegram: <a class="${css.linkStyle}" target="_blank" href="https://t.me/CardanoDevelopersOfficial">CardanoDevelopersOfficial</a> channel</li>
+            <li>Email technical support: <a class="${css.linkStyle}" href="mailto:testnet.goguen@iohk.io">testnet.goguen@iohk.io</a></li>
+            <li>Stack Exchange: <a class="${css.linkStyle}" target="_blank" href="https://area51.stackexchange.com/proposals/118211/cardano/118216#11821">Cardano StackExchange</a></li>
+          </ul>
         </div>
       </div>`
     if (!self._view.el) self._view.el = el
     return el
-    function openLink () { window.open('https://gitter.im/ethereum/remix') }
   }
 }
 
@@ -56,42 +52,8 @@ const css = csjs`
     flex-direction: column;
     overflow: hidden;
   }
-  .chat {
-    ${styles.rightPanel.supportTab.box_IframeContainer}
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 85%;
-    padding: 0;
-  }
-  .chatTitle {
-    height: 40px;
-    width: 90%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 15px;
-  }
-  .chatTitle:hover {
-    cursor: pointer;
-  }
-  .icon {
-    height: 70%;
-    margin-right: 2%;
-  }
-  .chatTitleText {
-    font-size: 17px;
-    font-weight: bold;
-  }
-  .chatTitleText {
-    opacity: 0.8;
-  }
-  .chatIframe {
-    width: 100%;
-    height: 100%;
-    transform: scale(0.9);
-    padding: 0;
-    border: none;
+  .linkList {
+    padding-left: 3em;
   }
   .linkStyle {
     color: ${styles.appProperties.specialText_Color}
